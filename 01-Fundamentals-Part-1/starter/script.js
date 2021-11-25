dbe3761e0ada78c06fc6cedf0a3171ca01efafee
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-11-25 16:44:54 +1300

    .

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,41 @@
+// const js = 'amazing'
+// if (js === 'amazing') alert('jeff')
+
+// console.log('jeff')
+
+// const firstName = 'Jeff'
+// console.log(firstName)
+
+// true
+// const jsbool = true
+// console.log(jsbool)
+
+// console.log(typeof true)
+
+// let variable = true
+// variable = 'jeff'
+// console.log(typeof variable)
+
+const markBMIOne = 78 / 1.69 ** 2
+console.log(markBMIOne)
+
+const markBMITwo = 95 / 1.88 ** 2
+console.log(markBMITwo)
+
+const johnBMIOne = 92 / 1.95 ** 2
+console.log(johnBMIOne)
+
+const johnBMITwo = 85 / 1.76 ** 2
+console.log(johnBMITwo)
+
+if (markBMIOne > johnBMIOne) {
+	console.log(markBMIOne)
+} else {
+	console.log(johnBMIOne)
+}
+
+if (markBMITwo > johnBMITwo) {
+	console.log(markBMITwo)
+} else {
+	console.log(johnBMITwo)
+}
