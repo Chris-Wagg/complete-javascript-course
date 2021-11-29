4a3f2903485a13e1816bc7cf873dfd390e6ad629
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-11-29 16:39:57 +1300

    .

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -16,26 +16,151 @@
 // variable = 'jeff'
 // console.log(typeof variable)
 
-const markBMIOne = 78 / 1.69 ** 2
-console.log(markBMIOne)
+// const markBMIOne = 78 / 1.69 ** 2
+// // console.log(markBMIOne)
 
-const markBMITwo = 95 / 1.88 ** 2
-console.log(markBMITwo)
+// const markBMITwo = 95 / 1.88 ** 2
+// // console.log(markBMITwo)
 
-const johnBMIOne = 92 / 1.95 ** 2
-console.log(johnBMIOne)
+// const johnBMIOne = 92 / 1.95 ** 2
+// // console.log(johnBMIOne)
 
-const johnBMITwo = 85 / 1.76 ** 2
-console.log(johnBMITwo)
+// const johnBMITwo = 85 / 1.76 ** 2
+// // console.log(johnBMITwo)
 
-if (markBMIOne > johnBMIOne) {
-	console.log(markBMIOne)
-} else {
-	console.log(johnBMIOne)
-}
+// if (markBMIOne > johnBMIOne) {
+// 	console.log(
+// 		`Marks first BMI is ${markBMIOne} which is higher than Johns ${johnBMIOne}`
+// 	)
+// } else {
+// 	console.log(
+// 		`Johns first bmi is ${johnBMIOne} which is higher than Marks ${markBMIOne}`
+// 	)
+// }
 
-if (markBMITwo > johnBMITwo) {
-	console.log(markBMITwo)
-} else {
-	console.log(johnBMITwo)
-}
+// if (markBMITwo > johnBMITwo) {
+// 	console.log(
+// 		`Marks second BMI is ${markBMITwo} which is higher than Johns ${johnBMITwo}`
+// 	)
+// } else {
+// 	console.log(
+// 		`Johns second bmi is ${johnBMITwo} which is higher than Marks ${markBMITwo}`
+// 	)
+// }
+
+// const jeff = 'Jeff'
+// const age = 69
+// const job = 'jeffer'
+
+// const test = `I'm ${jeff}, a ${age} year old ${job}!`
+
+// console.log(test)
+
+// console.log(`test
+// string
+// on
+// many
+// lines`)
+
+// const driveage = 14
+// const legal = driveage >= 18
+
+// if (legal) {
+// 	console.log(`you can drive 🚗🚗🚗`)
+// } else {
+// 	console.log(`you can't drive noob, you have ${18 - driveage} years left`)
+// }
+
+// const birthYear = 1994
+
+// let century
+// if (birthYear <= 2000) {
+// 	century = 20
+// } else {
+// 	century = 21
+// }
+
+// console.log(century)
+
+// console.log(Number('18'))
+// console.log(typeof NaN)
+// console.log(String(23), 23)
+
+// const money = 0
+// //0 is a falsey value
+// if (money) {
+// 	console.log(true)
+// } else {
+// 	console.log(false)
+// }
+
+// const moneyTwo = NaN
+// //numbers are truthy values
+// if (moneyTwo) {
+// 	console.log(true)
+// } else if (moneyTwo === false) {
+// 	console.log(false)
+// } else {
+// 	console.log(`jeff`)
+// }
+
+// == will type convert
+
+// == vs ===
+// != vs !==
+
+//&&
+// ||
+
+// if (a && b), if (a || b)
+// if ( a && b && !c)
+
+// const scoreOne = Math.floor((96 + 108 + 89) / 3)
+// const scoreTwo = Math.floor((88 + 91 + 110) / 3)
+
+// console.log(`scoreOne is ${scoreOne}`)
+// console.log(`scoreTwo is ${scoreTwo}`)
+
+// if (scoreOne === scoreTwo) {
+// 	console.log(`it's a tie with ${scoreOne} points each`)
+// } else if (scoreOne > scoreTwo) {
+// 	console.log(`scoreOne wins with ${scoreOne} points`)
+// } else {
+// 	console.log(`scoreTwo wins with ${scoreTwo} points`)
+// }
+
+// const scoreOneBonus = Math.floor((97 + 112 + 101) / 3)
+// const scoreTwoBonus = Math.floor((109 + 95 + 123) / 3)
+
+// console.log(`scoreOneBonus is ${scoreOneBonus}`)
+// console.log(`scoreTwoBonus is ${scoreTwoBonus}`)
+
+// if (scoreOneBonus === scoreTwoBonus) {
+// 	console.log(`it's a tie with ${scoreOneBonus} points each`)
+// } else if (scoreOneBonus > scoreTwoBonus && scoreOneBonus >= 100) {
+// 	console.log(`scoreOneBonus wins with ${scoreOneBonus} points`)
+// } else if (scoreTwoBonus >= 100) {
+// 	console.log(`scoreTwoBonus wins with ${scoreTwoBonus} points`)
+// } else {
+// 	console.log(`no one wins`)
+// }
+
+// const bonusDataTwoD = Math.floor((97 + 112 + 101) / 3)
+// const bonusDataTwoK = Math.floor((109 + 95 + 106) / 3)
+
+// console.log(`bonusDataTwoD is ${bonusDataTwoD}`)
+// console.log(`bonusDataTwoK is ${bonusDataTwoK}`)
+
+// if (
+// 	bonusDataTwoD === bonusDataTwoK &&
+// 	bonusDataTwoD >= 100 &&
+// 	bonusDataTwoK >= 100
+// ) {
+// 	console.log(`it's a tie with ${bonusDataTwoD} points each`)
+// } else if (bonusDataTwoD > bonusDataTwoK && bonusDataTwoD >= 100) {
+// 	console.log(`bonusDataTwoD wins with ${bonusDataTwoD} points`)
+// } else if (bonusDataTwoK >= 100) {
+// 	console.log(`scoreTwoBonus wins with ${bonusDataTwoK} points`)
+// } else {
+// 	console.log(`no one wins`)
+// }
