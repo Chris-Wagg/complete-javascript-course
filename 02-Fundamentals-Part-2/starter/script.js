c92294ce4ea0ad02cec4f734ce7c6e897e88b2b2
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-11-30 16:12:35 +1300

    .

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,64 @@
+'use strict'
+
+// function call() {
+// 	console.log(`jeff`)
+// }
+
+// call()
+
+// //a,b are arguments that can be passed in
+// function test(a, b) {
+// 	const foo = `jeff with ${a} and jeff with ${b}` // a and b are taken from where the function is called
+// 	return foo
+// }
+
+// const bar = test(1, 2) // this captures the return value of the function
+// console.log(bar)
+
+// this is a named function. this can be called before it's defined because of hoisting
+
+// function add1(num) {
+// 	return 2021 - num
+// }
+
+// const findNumber = add1(27)
+// console.log(findNumber)
+
+// // this is a anonymous function
+
+// const findDate = function (num) {
+// 	return 2021 - num
+// }
+
+// const date = findDate(28)
+// console.log(date)
+
+// functions are just values that can be stored in varaibles
+
+// //arrow function =>
+
+// const calcAge3 = (birthYear) => 2037 - birthYear
+// const age3 = calcAge3(1991)
+// console.log(age3)
+
+// const years = (birthyear, firstName) => {
+// 	const age = 2037 - birthyear
+// 	const retirement = 65 - age
+// 	return `${retirement}, ${firstName}`
+// }
+
+// console.log(years(1991, 'jeff'))
+
+// const cut = function (fruit) {
+// 	return fruit * 364
+// }
+
+// const process = function (a, b) {
+// 	const aPiece = cut(a)
+// 	const bPiece = cut(b)
+
+// 	const total = `jeff with ${aPiece} and ${bPiece}`
+// 	return total
+// }
+
+// console.log(process(1, 2))
