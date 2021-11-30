3cb88e646d8043dc4a731278794fca3f46fa8830
Chris-Wagg <cgwagg@hotmail.co.nz>
2021-12-01 11:38:33 +1300

    array

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -62,3 +62,67 @@
 // }
 
 // console.log(process(1, 2))
+
+// const dPointsOne = 44 + 23 + 71
+// const dPointsTwo = 85 + 54 + 41
+
+// const kPointsOne = 65 + 54 + 49
+// const kPointsTwo = 23 + 34 + 27
+
+// const calcAverage = (scores) => scores / 3
+
+// function checkWinner(avgD, avgK) {
+// 	const averageD = calcAverage(avgD)
+// 	const averageK = calcAverage(avgK)
+// 	console.log(averageD, averageK)
+// 	if (averageD >= 2 * averageK) {
+// 		console.log(`D wins with ${averageD} points`)
+// 	} else if (averageK >= 2 * averageD) {
+// 		console.log(`K wins with ${averageK} points`)
+// 	} else {
+// 		console.log(`no one wins`)
+// 	}
+// }
+
+// checkWinner(dPointsOne, kPointsOne)
+// checkWinner(dPointsTwo, kPointsTwo)
+
+// const array = [1, 2, 3, 4, 5]
+
+// console.log(array[2])
+// console.log(array.length)
+
+// console.log(array[array.length - 1])
+
+// array[2] = 'jeff'
+// console.log(array)
+
+// const numbers = [1 + 2, 2 + 3, 3 + 4]
+// console.log(numbers)
+
+// const arrayNest = [1, 2, 3, array]
+// console.log(arrayNest)
+
+const array = [1, 2, 3, 4, 5]
+const length = array.push('Jeff') // adds to end
+console.log(array)
+console.log(length)
+
+array.unshift('jeff')
+console.log(array)
+
+array.pop()
+console.log(array)
+
+const popped = array.pop()
+console.log(popped)
+// reutrns the popped element
+
+array.shift()
+console.log(array)
+
+console.log(array.indexOf(3))
+
+array.push('jeff')
+console.log(array)
+console.log(array.includes('jeff'))
