ed0be2cb6a20da3f1b7ca03fb9f3d281c7e0ba0f
Chris-Wagg <cgwagg@hotmail.co.nz>
2022-02-03 12:59:19 +0100

    Inheritence and prototyping

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -38,7 +38,7 @@ console.log(testOne)
 console.log(Person.prototype)
 
 // makes only one copy
-Person.prototype.calcAge = function () {
+Person.prototype.calcAge = function () { // protoype of objects created by person
     console.log(2022 - this.year)
 }
 
@@ -52,3 +52,35 @@ console.log(test.species)
 //check for properties
 console.log(test.hasOwnProperty('first')) // true because it's on the object
 console.log(test.hasOwnProperty('species')) // false because it's not on the object, just has access through the prototype
+
+// the newly created object is linked to the constructor functions prototype property
+
+
+// --------------- inheritence -----------------
+
+console.log(test.__proto__)
+console.dir(Person.prototype.constructor)
+
+const arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+console.log(arr.__proto__)
+
+console.log(arr.__proto__ === Array.prototype)
+
+// prototype property of the constructor is the prototype of all the objects created by that constructor
+
+
+// can add a new method to the prototype, all arrays will then inherit this method
+// not really a good idea though 
+Array.prototype.unique = function () {
+    return [...new Set(this)]
+}
+
+console.log(arr.unique())
+
+
+
+const h1 = document.querySelector('h1')
+
+console.dir(h1) // prototype is an html element with a big prototype chain
+
+// can call methods on functions because they are just objects with prototypes
